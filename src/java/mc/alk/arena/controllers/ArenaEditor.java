a848e7af206cf1ed3c914d27b5ef2262357c62c3
Europia79 <nikolai.kalashnikov@hotmail.com>
2014-09-28 23:20:32 -0500

    Fixed reference to private addTimedSpawn()
    This is the first "fix" to the reference to
    
    Arena.java
    private addTimedSpawn(Long index, TimedSpawn s);
    
    to
    
    public addTimedSpawn(TimedSpawn s);
    
    Passing an index is NOT necessary for an add() method.
    
    It's only necessary for put() and replacement methods.

--- src/java/mc/alk/arena/controllers/ArenaEditor.java
+++ src/java/mc/alk/arena/controllers/ArenaEditor.java
@@ -83,7 +83,7 @@ public class ArenaEditor implements Listener{
         }
         TimedSpawn ts = SpawnSerializer.createTimedSpawn(bs, cs.listeningOptions);
 
-        a.addTimedSpawn(cs.listeningIndex,ts);
+        a.addTimedSpawn(ts); // a.addTimedSpawn(cs.listeningIndex,ts);
         BattleArena.getBAController().updateArena(a);
         ArenaSerializer.saveArenas(a.getArenaType().getPlugin());
         MessageUtil.sendMessage(event.getPlayer(), "&2Added block spawn &6"+ ts +"&2 to index=&5"+cs.listeningIndex);
