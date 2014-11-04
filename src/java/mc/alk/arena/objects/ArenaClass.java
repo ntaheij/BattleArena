f2e775dc3c501aec59f5796d395f509a1d8d2e26
Europia79 <nikolai.kalashnikov@hotmail.com>
2014-11-03 21:32:10 -0600

    Added permissions attribute.
    The ArenaClass now has an attribute for permissions.
    However, this is not yet fully implemented.
    
    Permission nodes are not yet given or taken away from players.

--- src/java/mc/alk/arena/objects/ArenaClass.java
+++ src/java/mc/alk/arena/objects/ArenaClass.java
@@ -12,8 +12,8 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ArenaClass {
 	public static final Integer DEFAULT = Integer.MAX_VALUE;
-	public static final ArenaClass CHOSEN_CLASS = new ArenaClass("CHOSENCLASS","chosenClass", null, null);
-	public static final ArenaClass SELF_CLASS = new ArenaClass("SELFCLASS","selfClass", null, null);
+	public static final ArenaClass CHOSEN_CLASS = new ArenaClass("CHOSENCLASS","chosenClass", null, null, null);
+	public static final ArenaClass SELF_CLASS = new ArenaClass("SELFCLASS","selfClass", null, null, null);
 
 	/** Name of the Class*/
 	final String name;
@@ -35,15 +35,18 @@ public class ArenaClass {
 
 	/** List of commands to run when class is given */
 	List<CommandLineString> commands;
+        
+        /** List of permission nodes that this class has */
+        List<String> permissions;
 
 	boolean valid = false;
 
     public ArenaClass(String name){
-		this(name,name,new ArrayList<ItemStack>(),new ArrayList<PotionEffect>());
+		this(name,name,new ArrayList<ItemStack>(),new ArrayList<PotionEffect>(),new ArrayList<String>());
 		valid = false;
 	}
 
-	public ArenaClass(String name, String displayName, List<ItemStack> items, List<PotionEffect> effects){
+	public ArenaClass(String name, String displayName, List<ItemStack> items, List<PotionEffect> effects, List<String> perms){
 		this.name = name;
 		CopyOnWriteArrayList<ItemStack> listitems = new CopyOnWriteArrayList<ItemStack>();
 		ArrayList<ItemStack> armoritems = new ArrayList<ItemStack>();
@@ -60,6 +63,7 @@ public class ArenaClass {
 		this.items.addAll(armoritems);
 		this.effects = effects;
 		this.displayName = displayName;
+                this.permissions = perms;
 		valid = true;
 	}
 
@@ -145,5 +149,9 @@ public class ArenaClass {
             }
         }
         return l;
+    }
+    
+    public List<String> getPermissions() {
+        return permissions;
     }
 }
