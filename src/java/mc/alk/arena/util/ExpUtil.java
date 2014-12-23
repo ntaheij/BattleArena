17b5c074668931b003b54f2d7afac7371d5b1f46
Ali Moghnieh <SupaHam.me@gmail.com>
2014-12-23 00:36:26 +0000

    Revert "ExpUtil.getTotalExperience(Player) is now a delegate method for Player.getTotalExperience()."
    This reverts commit 56d987f98de93c8c4828ac16e7f2331abe262f7f.

--- src/java/mc/alk/arena/util/ExpUtil.java
+++ src/java/mc/alk/arena/util/ExpUtil.java
@@ -26,9 +26,7 @@ public class ExpUtil {
 	 * @return
 	 */
 	public static int getTotalExperience(Player p){
-	  	// This method is kept as a legacy thing, this used the calculations below.
-	  	// but was inaccurate, thus it was changed to use bukkit's method.
-		return p.getTotalExperience();
+		return getTotalExperience(p.getLevel(),p.getExp());
 	}
 
 	/**
