7b3bebd32a6a73e701797e8138f9e8baf53f9a43
Europia79 <nikolai.kalashnikov@hotmail.com>
2015-01-15 20:24:44 -0600

    Fixed ModuleLoader.
    Want to make ANY arena also a Paintball arena ? Use an ArenaModule.

--- src/java/mc/alk/arena/serializers/ModuleLoader.java
+++ src/java/mc/alk/arena/serializers/ModuleLoader.java
@@ -1,10 +1,5 @@
 package mc.alk.arena.serializers;
 
-import mc.alk.arena.controllers.ModuleController;
-import mc.alk.arena.objects.modules.ArenaModule;
-import mc.alk.arena.util.Log;
-import org.apache.commons.lang.StringUtils;
-
 import java.io.File;
 import java.io.FilenameFilter;
 import java.lang.reflect.Constructor;
@@ -12,58 +7,68 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
+import mc.alk.arena.controllers.Modules;
+import mc.alk.arena.modules.Grenades;
+import mc.alk.arena.modules.Paintballs;
+import mc.alk.arena.objects.modules.ArenaModule;
+import mc.alk.arena.util.Log;
+import org.apache.commons.lang.StringUtils;
 
 public class ModuleLoader {
-	public void loadModules(File moduleDirectory) {
-		if (!moduleDirectory.exists()){
-			return;
-		}
-		List<String> loadedModules = new ArrayList<String>();
-		for(File f:moduleDirectory.listFiles(new FilenameFilter(){
-			@Override
-			public boolean accept(File dir, String name) {
-				int period = name.lastIndexOf('.');
-				final String sname = name.substring(period + 1);
-				return period != -1 && sname.equals("class") || sname.equals("jar");
-			}}))
-		{
-			ArenaModule am = null;
-			try {
-				am = loadModule(f);
-				loadedModules.add(am.getName() +"_"+am.getVersion());
-				am.setEnabled(true);
-				am.onEnable();
-				ModuleController.addModule(am);
-			} catch (Exception e){
-				Log.err("[BA Error] Error loading the module " + f.getName());
-				if (am != null){
-					am.setEnabled(false);
-				}
-				Log.printStackTrace(e);
-			}
-		}
-		if (loadedModules.isEmpty()){
-			Log.info("[BattleArena] no additional Arena modules");
-		} else {
-			Log.info("[BattleArena] loaded modules ["+StringUtils.join(loadedModules,", ")+"]");
-		}
 
-	}
+    public void loadModules(File moduleDirectory) {
+        List<String> loadedModules = new ArrayList<String>();
+        for (ArenaModule mod : new ArenaModule[]{new Paintballs(), new Grenades()}) {
+            Modules.addModule(mod);
+            loadedModules.add(mod.getName());
+            mod.setEnabled(true);
+        }
+        if (!moduleDirectory.exists()) {
+            return;
+        }
 
-	private ArenaModule loadModule(File f) throws Exception{
-		ClassLoader loader = this.getClass().getClassLoader();
-		URL url = f.toURI().toURL();
+        for (File mod : moduleDirectory.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                int period = name.lastIndexOf('.');
+                final String extension = name.substring(period + 1);
+                return period != -1 && extension.equals("class") || extension.equals("jar");
+            }
+        })) {
+            ArenaModule am = null;
+            try {
+                am = loadModule(moduleDirectory, mod);
+                loadedModules.add(am.getName());
+                am.setEnabled(true);
+                am.onEnable();
+                Modules.addModule(am);
+            } catch (Exception ex) {
+                Log.err("[BA Error] Error loading the module " + mod.toString());
+                if (am != null) {
+                    am.setEnabled(false);
+                }
+                Log.printStackTrace(ex);
+            }
+        }
+        Log.info(Log.colorChat("[BattleArena] Modules (" + loadedModules.size() + ") [&a" + StringUtils.join(loadedModules, "&f, &a") + "&f]"));
 
-		URL[] urls = {url};
-		URLClassLoader ucl = new URLClassLoader(urls,loader);
+    }
 
-		//Load the class
-		String shortName = f.getName().substring(0,f.getName().indexOf('.'));
-		Class<?> clazz = ucl.loadClass(shortName);
+    private ArenaModule loadModule(File dir, File mod) throws Exception {
+        ClassLoader loader = this.getClass().getClassLoader();
+        URL url = dir.toURI().toURL();
 
-		Class<?>[] args = {};
-		Class<?extends ArenaModule> moduleClass = clazz.asSubclass(ArenaModule.class);
-		Constructor<?> constructor = moduleClass.getConstructor(args);
-        return (ArenaModule) constructor.newInstance((Object[])args);
-	}
+        URL[] urls = {url};
+        URLClassLoader ucl = new URLClassLoader(urls, loader);
+
+        //Load the class
+        String shortName = mod.getName().substring(0, mod.getName().indexOf('.'));
+        System.out.println("ArenaModule::loadModule(" + mod.getName() + "); // shortName = " + shortName);
+        System.out.println("dir.toURI().toURL() = " + dir.toURI().toURL());
+        Class<?> clazz = ucl.loadClass(shortName);
+        Class<?>[] args = {};
+        Class<? extends ArenaModule> moduleClass = clazz.asSubclass(ArenaModule.class);
+        Constructor<?> constructor = moduleClass.getConstructor(args);
+        return (ArenaModule) constructor.newInstance((Object[]) args);
+    }
 }
