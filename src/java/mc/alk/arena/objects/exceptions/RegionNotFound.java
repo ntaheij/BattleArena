934ee1f27145882c87dc5eef71346baec47f08b9
Europia79 <nikolai.kalashnikov@hotmail.com>
2015-08-31 15:01:17 -0500

    Removed duplicate class
    mc.alk.arena.objects.exceptions.RegionNotFound;
    
    This class is included with mc.alk:worldeditutil

--- src/java/mc/alk/arena/objects/exceptions/RegionNotFound.java
+++
@@ -1,8 +0,0 @@
-package mc.alk.arena.objects.exceptions;
-
-public class RegionNotFound extends Exception{
-	private static final long serialVersionUID = 1L;
-	public RegionNotFound(String msg) {
-		super(msg);
-	}
-}
