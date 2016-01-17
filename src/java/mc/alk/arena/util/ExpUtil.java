0d7031afb5a86f6aec7878421e4bd866c0a96881
Europia79 <nikolai.kalashnikov@hotmail.com>
2016-01-17 17:19:20 -0600

    Added javadoc comments

--- src/java/mc/alk/arena/util/ExpUtil.java
+++ src/java/mc/alk/arena/util/ExpUtil.java
@@ -3,11 +3,11 @@ package mc.alk.arena.util;
 import org.bukkit.entity.Player;
 
 /**
+ * Never use player.getTotalExperience() because it can return an incorrect value 
+ * because Bukkit does not update experience after spending it on enchants.
+ * 
  * @author alkarin
  * version: 1.4
- *
- * This class is strictly used because Bukkit DOES NOT UPDATE experience after enchanting
- * so using player.getTotalExperience() returns an INCORRECT amount
  *
  * Levels based off of 1.3 exp formulas verified by myself.
  * Formulas used.
@@ -20,8 +20,10 @@ import org.bukkit.entity.Player;
  */
 public class ExpUtil {
 
-    //This method is required because the bukkit player.getTotalExperience() method, shows exp that has been 'spent'.
-    //Without this people would be able to use exp and then still sell it.
+    /**
+     * Never use player.getTotalExperience(), use this method instead.
+     * player.getTotalExperience() shows XP that has been spent on enchants.
+     */
     public static int getTotalExperience(final Player player) {
 	int exp = (int)Math.round(getExpAtLevel(player.getLevel()) * player.getExp());
 	int currentLevel = player.getLevel();
@@ -36,8 +38,10 @@ public class ExpUtil {
 	return exp;
     }
 
-    //new Exp Math from 1.8
-    // Credits go to andrewkm for this code
+    /**
+     * New XP math for Minecraft 1.8
+     * Credits go to andrewkm for the 1.8 code
+     */
     public static int getExpAtLevel(final int level) {
         if (level <= 15) {
           return (2*level) + 7;
