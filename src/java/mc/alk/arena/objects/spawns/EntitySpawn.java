726107c02cf7df7f1609b4e16004cb7ce4b38e4b
Europia79 <nikolai.kalashnikov@hotmail.com>
2016-03-20 13:43:01 -0500

    BA v3.9.10.8 Fixes #68

--- src/java/mc/alk/arena/objects/spawns/EntitySpawn.java
+++ src/java/mc/alk/arena/objects/spawns/EntitySpawn.java
@@ -1,32 +1,34 @@
 package mc.alk.arena.objects.spawns;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
 import mc.alk.arena.objects.ArenaPlayer;
 import mc.alk.arena.util.EntityUtil;
 import mc.alk.arena.util.Log;
 import mc.alk.arena.util.TeamUtil;
+
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.AnimalTamer;
 import org.bukkit.entity.Creature;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Tameable;
 import org.bukkit.entity.Wolf;
 
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
-
 public class EntitySpawn extends SpawnInstance{
     final private EntityType et;
-    final List<LivingEntity> uids = new ArrayList<LivingEntity>();
+    final List<Entity> uids = new ArrayList<Entity>();
     int number = 1;
     ArenaPlayer owner;
 
     static Method spawnEntityMethod;
     static {
         try {
-            spawnEntityMethod = World.class.getMethod("spawnEntity");
+            spawnEntityMethod = World.class.getMethod("spawnEntity", Location.class, EntityType.class);
         } catch (Exception e) {
             try {
                 spawnEntityMethod = World.class.getMethod("spawnCreature", Location.class, EntityType.class);
@@ -57,19 +59,19 @@ public class EntitySpawn extends SpawnInstance{
     public void spawn() {
         if (spawnEntityMethod==null)
             return;
-        for (LivingEntity id: uids){
+        for (Entity id: uids){
             if (!id.isDead()){
                 return;} /// The entities are already spawned
         }
         uids.clear();
         for (int i=0;i< number;i++) {
             try {
-                LivingEntity le = (LivingEntity) spawnEntityMethod.invoke(loc.getWorld(), loc, et);
-                if (le instanceof Wolf && owner != null && owner.getTeam()!=null) {
-                    EntityUtil.setCollarColor((Wolf) le,
+                Entity entity = (Entity) spawnEntityMethod.invoke(loc.getWorld(), loc, et);
+                if (entity instanceof Wolf && owner != null && owner.getTeam()!=null) {
+                    EntityUtil.setCollarColor((Wolf) entity,
                             TeamUtil.getDyeColor(owner.getTeam().getIndex()));
                 }
-                uids.add(le);
+                uids.add(entity);
             } catch (Exception e) {
                 Log.printStackTrace(e);
             }
@@ -78,7 +80,7 @@ public class EntitySpawn extends SpawnInstance{
 
     @Override
     public void despawn() {
-        for (LivingEntity id: uids){
+        for (Entity id: uids){
             if (!id.isDead()){
                 id.remove();
             }
@@ -92,7 +94,7 @@ public class EntitySpawn extends SpawnInstance{
     }
 
     public void setOwner(AnimalTamer tamer){
-        for (LivingEntity le: uids){
+        for (Entity le: uids){
             if (!le.isDead()){
                 if (le instanceof Tameable){
                     ((Tameable)le).setTamed(true);
@@ -123,7 +125,7 @@ public class EntitySpawn extends SpawnInstance{
     }
 
     public void setTarget(LivingEntity entity) {
-        for (LivingEntity id: uids){
+        for (Entity id: uids){
             if (!id.isDead()){
                 if (id instanceof Creature){
                     ((Creature)id).setTarget(entity);
