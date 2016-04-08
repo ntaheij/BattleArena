01e90724238e564d1288a7965ab6d29ee59c0196
Europia79 <nikolai.kalashnikov@hotmail.com>
2016-04-07 20:43:20 -0500

    Formatted CommandUtil.java
    issue #72:
    disabledCommands set to ["all"]
    enabledCommands set to [arena leave]
    
    Command "/arena leave" isn't allowed.
    
    All commands sould be disabled with the exception of cmd "/arena leave"

--- src/java/mc/alk/arena/util/CommandUtil.java
+++ src/java/mc/alk/arena/util/CommandUtil.java
@@ -7,25 +7,29 @@ import java.util.Set;
 
 public class CommandUtil {
 
-	public static boolean shouldCancel(PlayerCommandPreprocessEvent event, boolean allDisabled,
-                                       Set<String> disabledCommands, Set<String> enabledCommands){
-		if (Defaults.DEBUG_COMMANDS){
-			event.getPlayer().sendMessage("event Message=" + event.getMessage() +"   isCancelled=" + event.isCancelled());}
-		if (!allDisabled && (disabledCommands == null || disabledCommands.isEmpty()))
-			return false;
-		if (Defaults.ALLOW_ADMIN_CMDS_IN_Q_OR_MATCH && PermissionsUtil.isAdmin(event.getPlayer())){
-			return false;}
-        if (allDisabled && (enabledCommands.isEmpty()))
+    public static boolean shouldCancel(PlayerCommandPreprocessEvent event, boolean allDisabled,
+            Set<String> disabledCommands, Set<String> enabledCommands) {
+        if (Defaults.DEBUG_COMMANDS) {
+            event.getPlayer().sendMessage("event Message=" + event.getMessage() + "   isCancelled=" + event.isCancelled());
+        }
+        if (!allDisabled && (disabledCommands == null || disabledCommands.isEmpty())) {
+            return false;
+        }
+        if (Defaults.ALLOW_ADMIN_CMDS_IN_Q_OR_MATCH && PermissionsUtil.isAdmin(event.getPlayer())) {
+            return false;
+        }
+        if (allDisabled && (enabledCommands.isEmpty())) {
             return true;
+        }
 
-		String cmd = event.getMessage();
-		final int index = cmd.indexOf(' ');
-		if (index != -1){
-			cmd = cmd.substring(0, index);
-		}
+        String cmd = event.getMessage();
+        final int index = cmd.indexOf(' ');
+        if (index != -1) {
+            cmd = cmd.substring(0, index);
+        }
         cmd = cmd.toLowerCase();
 //        boolean enabled = ;
-        return !(enabledCommands != null && enabledCommands.contains(cmd)) &&
-                !cmd.equals("/bad") && (allDisabled || disabledCommands.contains(cmd));
+        return !(enabledCommands != null && enabledCommands.contains(cmd))
+                && !cmd.equals("/bad") && (allDisabled || disabledCommands.contains(cmd));
     }
 }
